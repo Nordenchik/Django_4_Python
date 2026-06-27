cd029d23944c6ad3b96c261d6bb6a8eaf5fd2a33
Nordenchik <nordenchik228@gmail.com>
2026-06-27 13:23:13 +0300

    v1.1.0

--- booking/wsgi.py
+++ booking/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for demo project.
+WSGI config for booking project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking.settings')
 application = get_wsgi_application()
