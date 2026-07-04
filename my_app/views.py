6730eab9ffac85b6cc487b2b23fe592f53af807b
Nordenchik <nordenchik228@gmail.com>
2026-07-04 14:13:10 +0300

    v2.0.1

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Rooms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, login
 
 def rooms_list(request):
     rooms = Rooms.objects.all()
@@ -10,3 +11,13 @@ def room_details(request, pk):
     room = get_object_or_404(Rooms, pk=pk)
     context = {'room' : room}
     return render(request, 'room_details.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            user = form.get_user()
+            login(request, user)
+            return redirect('rooms_list')
+    else: form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
